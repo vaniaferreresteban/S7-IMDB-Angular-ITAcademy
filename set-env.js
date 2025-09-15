e1d95083ffcf200a8b5e0e84db4b1e712628a398
Vania Serra <vaniaferreresteban@gmail.com>
2025-09-15 19:55:51 +0200

    fix: restore set-env.js for environment variable management

---
+++ set-env.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+let targetPath;
+let envFileContent;
+
+if (isProduction) {
+  console.log('Generating environment.ts for production...');
+  targetPath = path.join(__dirname, './src/environments/environment.ts');
+  envFileContent = `export const environment = {
+  production: true,
+  IMDB_KEY: '${process.env.IMDB_KEY}',
+  SUPABASE_URL: '${process.env.SUPABASE_URL}',
+  SUPABASE_KEY: '${process.env.SUPABASE_KEY}'
+};
+`;
+} else {
+  require('dotenv').config();
+  console.log('Generating environment.development.ts for development...');
+  targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+  envFileContent = `export const environment = {
+  production: false,
+  IMDB_KEY: '${process.env.IMDB_KEY}',
+  SUPABASE_URL: '${process.env.SUPABASE_URL}',
+  SUPABASE_KEY: '${process.env.SUPABASE_KEY}'
+};
+`;
+}
+fs.writeFile(targetPath, envFileContent, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated ${path.basename(targetPath)}`);
+    }
+});
