075399a6f600ea5b19f13aa3426f16a72d06a620
vaniaferreresteban <vaniaferreresteban@gmail.com>
2025-09-15 14:35:36 +0200

    fix: Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  IMDB_KEY:
-    '',
-  SUPABASE_URL: '',
-  SUPABASE_KEY:
-    '',
+  IMDB_KEY:process.env['IMDB_KEY'] || '',
+  supabaseUrl: process.env['SUPABASE_URL'] || '',
+  supabaseKey: process.env['SUPABASE_KEY'] || '',
 };
