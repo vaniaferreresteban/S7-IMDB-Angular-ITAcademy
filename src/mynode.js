a7f1348ae865ddfee9e2af9cab598af9cebeb226
Vania Serra <vaniaferreresteban@gmail.com>
2025-09-15 19:30:59 +0200

    fix: update build script and improve environment variable handling in mynode.js

--- src/mynode.js
+++ src/mynode.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({ path: 'src/.env' });;
+const dotenv = require('dotenv').config({ path: 'src/.env' });
+
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const envFile = `export const environment = {
     IMDB_KEY: '${process.env.IMDB_KEY}',
@@ -10,7 +13,27 @@ const envFile = `export const environment = {
     SUPABASE_KEY: '${process.env.SUPABASE_KEY}'
 };
 `;
-const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+if (isProduction) {
+    targetPath = path.join(__dirname, './src/environments/environment.ts');
+    envFileContent = `export const environment = {
+  production: true,
+  IMDB_KEY: '${process.env.IMDB_KEY}',
+  SUPABASE_URL: '${process.env.SUPABASE_URL}',
+  SUPABASE_KEY: '${process.env.SUPABASE_KEY}'
+};
+`;
+} else {
+    require('dotenv').config();
+    targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+    envFileContent = `export const environment = {
+  production: false,
+  IMDB_KEY: '${process.env.IMDB_KEY}',
+  SUPABASE_URL: '${process.env.SUPABASE_URL}',
+  SUPABASE_KEY: '${process.env.SUPABASE_KEY}'
+};
+`;
+}
+
 fs.writeFile(targetPath, envFile, (err) => {
     if (err) {
         console.error(err);
