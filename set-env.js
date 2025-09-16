426c6019091593a3577eba697a57eb02c78410dd
vaniaferreresteban <vaniaferreresteban@gmail.com>
2025-09-16 11:04:37 +0200

    fix: update environment variable access to use import.meta.env

--- set-env.js
+++ set-env.js
@@ -1,5 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
 
@@ -13,28 +19,29 @@ if (isProduction) {
   targetPath = path.join(__dirname, './src/environments/environment.ts');
   envFileContent = `export const environment = {
   production: true,
-  IMDB_KEY: '${import.meta.env.NG_APP_IMDB_KEY}',
-  SUPABASE_URL: '${import.meta.env.NG_APP_SUPABASE_URL}',
-  SUPABASE_KEY: '${import.meta.env.NG_APP_SUPABASE_KEY}'
+  IMDB_KEY: '${import.meta.env.NG_APP_IMDB_KEY || ''}',
+  SUPABASE_URL: '${import.meta.env.NG_APP_SUPABASE_URL || ''}',
+  SUPABASE_KEY: '${import.meta.env.NG_APP_SUPABASE_KEY || ''}'
 };
 `;
 } else {
-  require('dotenv').config();
+  dotenv.config();
   console.log('Generating environment.development.ts for development...');
   targetPath = path.join(__dirname, './src/environments/environment.development.ts');
   envFileContent = `export const environment = {
   production: false,
-  IMDB_KEY: '${import.meta.env.NG_APP_IMDB_KEY}',
-  SUPABASE_URL: '${import.meta.env.NG_APP_SUPABASE_URL}',
-  SUPABASE_KEY: '${import.meta.env.NG_APP_SUPABASE_KEY}'
+  IMDB_KEY: '${import.meta.env.NG_APP_IMDB_KEY || ''}',
+  SUPABASE_URL: '${import.meta.env.NG_APP_SUPABASE_URL || ''}',
+  SUPABASE_KEY: '${import.meta.env.NG_APP_SUPABASE_KEY || ''}'
 };
 `;
 }
+
 fs.writeFile(targetPath, envFileContent, (err) => {
-    if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        console.log(successColor, `${checkSign} Successfully generated ${path.basename(targetPath)}`);
-    }
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(successColor, `${checkSign} Successfully generated ${path.basename(targetPath)}`);
+  }
 });
