8e6fe3b5c2082078c4a0e4b9cea792af0c9683bb
vaniaferreresteban <vaniaferreresteban@gmail.com>
2025-09-16 13:37:06 +0200

    fix: update environment variables for supabase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   IMDB_KEY: process.env['NG_APP_IMDB_KEY'] || '',
-  SUPABASE_URL: process.env['NG_APP_SUPABASE_URL'] || '',
-  SUPABASE_KEY: process.env['NG_APP_SUPABASE_KEY'] || '',
+  supabaseUrl: process.env['NG_APP_SUPABASE_URL'] || '',
+  supabaseKey: process.env['NG_APP_SUPABASE_KEY'] || '',
 };
